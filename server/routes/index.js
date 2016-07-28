5f0dced49fb4af80927f4ebeed61fdc18041d5fa
Robert Stires <rjstires@gmail.com>
2016-07-28 08:05:12 -0400

    Dynamic generation of routes v2.0

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,20 +1,39 @@
 /**
  * routes/index.js
  * Scan routes/ folder for all *.routes.js files and create a route
- * for it where the path is the word before .routes.js.
+ * for it where the path is the word before .routes.js (noun).
  */
 
 const router = require('express').Router(); // eslint-disable-line new-cap
 const readDir = require('readdir');
 const _ = require('lodash');
 const files = readDir.readSync(__dirname, ['*.routes.js']);
-let noun, routePath, routeModule;
+let routes = [];
 
+// Bould the collection.
 _.each(files, function(file) {
-  noun = file.substr(0, file.indexOf('.'));
-  routePath =(noun === 'auth') ? '/' : `/${noun}`;
-  routeModule = `./${file}`;
-  router.use(routePath, require(routeModule));
+  routes.push({
+    path: '/' + file.substr(0, file.indexOf('.')),
+    module: `./${file}`
+  });
 });
 
+// Overrides
+routes = routes.map(function(route) {
+  if(route.path === '/auth'){
+    return _.assign({}, route, {path: '/'});
+  }
+
+  return route;
+});
+
+// Sorting
+_.orderBy(routes, function(r) {
+  return r.path;
+});
+
+// Initialize the routes
+_.each(routes, function(route) {
+  router.use(route.path, require(route.module));
+});
 module.exports = router;
