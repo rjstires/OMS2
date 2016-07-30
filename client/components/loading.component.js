a10eb850ceb2e53a7da4121f03dceaf77aadb286
Robert Stires <rjstires@gmail.com>
2016-07-29 23:22:04 -0400

    Update loading component to flat dream style (which sucks)

--- client/components/loading.component.js
+++ client/components/loading.component.js
@@ -1,11 +1,10 @@
-import {React} from 'react';
+import React from 'react';
 
 const LoadingComponent = () => {
+  const style={width: '100%'};
   return(
-    <div className="col m4 offset-m4">
-      <div className="progress">
-        <div className="indeterminate"></div>
-      </div>
+    <div className="progress progress-striped active">
+      <div className="progress-bar progress-bar-info" style={style}>Loading...</div>
     </div>
   );
 };
